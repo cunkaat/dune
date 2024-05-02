63c2024f19a61bf5d827238d9475cbd4d37b0e4b
cunkaat <145510157+cunkaat@users.noreply.github.com>
2024-05-02 11:33:27 -0700

    dune 5

--- dune5.js
+++ dune5.js
@@ -30,19 +30,36 @@ function mouseOverCircle() {
 function brighter() {
   fill('#DDAA6F');
   noStroke();
-  ellipse(width/2, (height/2)+70, 700, 700);
+  ellipse(width / 2, height / 2, 700, 700);
   textSize(100);
   textFont('Helvetica');
   fill('white');
-  text('STOPPING', 860, 650);
-  text('IT.', 1080, 780);
+  text('STOPPING', 860, 590);
+  text('IT.', 1080, 700);
+
+  if (dist(mouseX, mouseY, width / 2 - 250, height / 2 - 250) < 115) {
+    ellipse1Fill = '#ADA092'; // Change fill color to #FFF4E3
+  } else {
+    ellipse1Fill = '#FFD19B'; // Revert fill color to initial color
+  }
+  fill(ellipse1Fill);
+  ellipse((width / 2) - 250, (height / 2) - 250, 230, 230);
+
+  // Check if the mouse is hovering over the second ellipse at (390, 310)
+  if (dist(mouseX, mouseY, width / 2 + 250, height / 2 + 250) < 115) {
+    ellipse2Fill = '#FFF4E3'; // Change fill color to #ADA092
+  } else {
+    ellipse2Fill = '#F2C48E'; // Revert fill color to initial color
+  }
+  fill(ellipse2Fill);
+  ellipse((width / 2) + 250, (height / 2) + 250, 230, 230);
   
   if (mouseX > 820 && mouseX < 1400 && mouseY > 550 && mouseY < 800) {
   textSize(100);
   textFont('Helvetica');
   fill('black');
-  text('STOPPING', 860, 650);
-  text('IT.', 1080, 780);
+  text('STOPPING', 860, 590);
+  text('IT.', 1080, 700);
     
   }
 }
