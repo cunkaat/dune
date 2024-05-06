693a56bfa5bc7165b356de4241df8578318fca8e
cunkaat <katrinacunanan25@gmail.com>
2024-05-06 12:48:43 -0700

    trying to fix gif but not working

--- dune_index.js
+++ dune_index.js
@@ -1,5 +1,6 @@
 var gif_createImg;
 var gif_createGif;
+var link; 
 
 function preload() {
   gif_createImg = createImg("dune_gif.gif");
@@ -12,6 +13,8 @@ function setup() {
 
   gif_createImg.size(windowWidth, windowHeight);
   gif_createGif.size(windowWidth, windowHeight);
+
+  // link = createA('https://cunkaat.github.io/dune/dune1.html')
   
 
 }
@@ -20,11 +23,16 @@ function draw() {
   background('black');
   gif_createImg.position(0, 0);
   gif_createGif.position(0, 0);
+  // link.position(width/2, height/2);
+  if (mouseisPressed) {
+    window.open('https://cunkaat.github.io/dune/dune1.html');
+  }
+
 }
 
-function mousePressed() {
-  if (dist(mouseX > width/2 - 100 && mouseX < width/2 + 100 && mouseY > height/2-100 && mouseY < height/2 +100)) {
-    window.open('https://cunkaat.github.io/dune/dune1.html')
-  }
+// function mousePressed() {
+//   if (mouseX > width/2 && mouseY < height/2) {
+//     window.open('https://cunkaat.github.io/dune/dune1.html')
+//   }
   
-}
+// }
