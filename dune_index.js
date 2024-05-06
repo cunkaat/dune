057b1ed878f887b9d513e822c51203c16a071d84
cunkaat <katrinacunanan25@gmail.com>
2024-05-06 00:49:37 -0700

    adjusted the gif size

--- dune_index.js
+++ dune_index.js
@@ -1,4 +1,5 @@
 var gif_createImg;
+var gif_createGif;
 
 function preload() {
   gif_createImg = createImg("dune_gif.gif");
@@ -8,11 +9,21 @@ function preload() {
 function setup() {
   createCanvas(windowWidth, windowHeight);
   background(0);
+
+  gif_createImg.size(windowWidth, windowHeight);
+  gif_createGif.size(windowWidth, windowHeight);
+  
+  // Add mouse click event listener to the canvas
+//   canvas.mouseClicked(openURL);
 }
 
 function draw() {
   background('black');
   gif_createImg.position(0, 0);
-  gif_createGif.position(0,60);
-
+  gif_createGif.position(0, 0);
 }
+
+// Function to open the specified URL
+// function openURL() {
+//   window.open("https://cunkaat.github.io/dune/dune1.html");
+// }
