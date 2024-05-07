ace558835525b8d500feb60e8c23ce221fff6588
cunkaat <145510157+cunkaat@users.noreply.github.com>
2024-05-07 10:21:47 -0700

    created forward and back arrows

--- dune6a.js
+++ dune6a.js
@@ -31,7 +31,14 @@ function draw() {
     ellipse1Fill = '#FFD19B'; // Revert fill color to initial color
   }
   fill(ellipse1Fill);
-  ellipse((width / 2) - 250, (height / 2) - 180, 230, 230);
+  ellipse((width / 2) - 250, (height / 2) - 170, 230, 230);
+  //back arrow
+  stroke('black');
+  strokeWeight(8);
+  line(((width / 2) - 280), ((height / 2) - 170),((width / 2) - 220), ((height / 2) - 170));
+  line(((width / 2) - 280), ((height / 2) - 170),((width / 2) - 260), ((height / 2) - 150));
+  line(((width / 2) - 280), ((height / 2) - 170),((width / 2) - 260), ((height / 2) - 190));
+  noStroke();
 
   // Check if the mouse is hovering over the second ellipse at (390, 310)
   if (dist(mouseX, mouseY, width / 2 + 250, height / 2 + 250) < 115) {
@@ -40,14 +47,22 @@ function draw() {
     ellipse2Fill = '#F2C48E'; // Revert fill color to initial color
   }
   fill(ellipse2Fill);
-  ellipse((width / 2) + 250, (height / 2) + 250+70, 230, 230);
+  ellipse((width / 2) + 250, (height / 2) + 200 + 70, 230, 230);
+
+  //forward arrow
+  stroke('black');
+  strokeWeight(8);
+  line(((width / 2) + 220), ((height / 2) + 270),((width / 2) + 280), ((height / 2) + 270));
+  line(((width / 2) + 280), (height / 2) + 270, ((width / 2) + 260), ((height / 2) + 290));
+  line(((width / 2) + 280), (height / 2) + 270, ((width / 2) + 260), ((height / 2) + 250));
+  noStroke();
 
   fill('WHITE');
   textSize(50);
   textStyle(BOLD);
   textFont('Helvetica');
-  text('UNDERSTANDING MUST MOVE', 720, 400);
-  text('OF THE PROCESS,', 900, 850);
+  text('UNDERSTANDING MUST MOVE', 720, 460);
+  text('OF THE PROCESS,', 900, 790);
 
   const tiles = 6;
   const tileSize = (600 / tiles) + 20;
